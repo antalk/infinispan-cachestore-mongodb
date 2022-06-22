6f8d7e3e3f0d833cde8ebfc281e19bc2d77cac04
Antal van Kalleveen <A.Kalleveen@stater.nl>
2022-06-22 16:33:36 +0200

    does not work yet..

--- src/main/java/org/infinispan/persistence/mongodb/configuration/parser/MongoDBCacheStoreConfigurationParser.java
+++ src/main/java/org/infinispan/persistence/mongodb/configuration/parser/MongoDBCacheStoreConfigurationParser.java
@@ -1,14 +1,16 @@
 package org.infinispan.persistence.mongodb.configuration.parser;
 
+import static org.infinispan.commons.util.StringPropertyReplacer.replaceProperties;
+
+import org.infinispan.commons.configuration.io.ConfigurationReader;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.configuration.cache.PersistenceConfigurationBuilder;
-import org.infinispan.configuration.parsing.*;
+import org.infinispan.configuration.parsing.ConfigurationBuilderHolder;
+import org.infinispan.configuration.parsing.ConfigurationParser;
+import org.infinispan.configuration.parsing.Namespace;
+import org.infinispan.configuration.parsing.ParseUtils;
+import org.infinispan.configuration.parsing.Parser;
 import org.infinispan.persistence.mongodb.configuration.MongoDBStoreConfigurationBuilder;
-
-import javax.xml.stream.XMLStreamConstants;
-import javax.xml.stream.XMLStreamException;
-
-import static org.infinispan.commons.util.StringPropertyReplacer.replaceProperties;
 
 /**
  * Parses the configuration from the XML. For valid elements and attributes refer to {@link Element} and {@link
@@ -23,8 +25,7 @@ import static org.infinispan.commons.util.StringPropertyReplacer.replaceProperti
 public class MongoDBCacheStoreConfigurationParser implements ConfigurationParser {
 
    @Override
-   public void readElement(XMLExtendedStreamReader reader, ConfigurationBuilderHolder configurationBuilderHolder)
-           throws XMLStreamException {
+   public void readElement(ConfigurationReader reader, ConfigurationBuilderHolder configurationBuilderHolder) {
       ConfigurationBuilder builder = configurationBuilderHolder.getCurrentConfigurationBuilder();
 
       Element element = Element.forName(reader.getLocalName());
@@ -45,11 +46,10 @@ public class MongoDBCacheStoreConfigurationParser implements ConfigurationParser
       return ParseUtils.getNamespaceAnnotations(getClass());
    }
 
-   private void parseMongoDBStore(XMLExtendedStreamReader reader, PersistenceConfigurationBuilder persistenceConfigurationBuilder)
-           throws XMLStreamException {
+   private void parseMongoDBStore(ConfigurationReader reader, PersistenceConfigurationBuilder persistenceConfigurationBuilder) {
       MongoDBStoreConfigurationBuilder builder = new MongoDBStoreConfigurationBuilder(persistenceConfigurationBuilder);
 
-      while (reader.hasNext() && (reader.nextTag() != XMLStreamConstants.END_ELEMENT)) {
+      while (reader.hasNext()) {
          Element element = Element.forName(reader.getLocalName());
          switch (element) {
             case CONNECTION: {
@@ -64,12 +64,11 @@ public class MongoDBCacheStoreConfigurationParser implements ConfigurationParser
       persistenceConfigurationBuilder.addStore(builder);
    }
 
-   private void parseConnection(XMLExtendedStreamReader reader, MongoDBStoreConfigurationBuilder builder)
-           throws XMLStreamException {
+   private void parseConnection(ConfigurationReader reader, MongoDBStoreConfigurationBuilder builder) {
       for (int i = 0; i < reader.getAttributeCount(); i++) {
          ParseUtils.requireNoNamespaceAttribute(reader, i);
          String value = replaceProperties(reader.getAttributeValue(i));
-         Attribute attribute = Attribute.forName(reader.getAttributeLocalName(i));
+         Attribute attribute = Attribute.forName(reader.getAttributeName(i));
          switch (attribute) {
             case CONNECTION_URI: {
                builder.connectionURI(value);
